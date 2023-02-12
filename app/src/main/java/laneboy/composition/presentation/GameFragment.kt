d05c2b5de2cafef514c3fa3728f78f60c03b46bb
LaneBoyy <kolomyjcevvladislav90@gmail.com>
2023-02-12 20:06:36 +0500

    Commit 10

--- app/src/main/java/laneboy/composition/presentation/GameFragment.kt
+++ app/src/main/java/laneboy/composition/presentation/GameFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import laneboy.composition.R
 import laneboy.composition.databinding.FragmentGameBinding
 import laneboy.composition.domain.entity.GameResult
-import laneboy.composition.domain.entity.GameSettings
 import laneboy.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
 
     private lateinit var level: Level
+
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        )[GameViewModel::class.java]
+    }
+
+    private val tvOptions by lazy {
+        mutableListOf<TextView>().apply {
+            add(binding.tvOption1)
+            add(binding.tvOption2)
+            add(binding.tvOption3)
+            add(binding.tvOption4)
+            add(binding.tvOption5)
+            add(binding.tvOption6)
+        }
+    }
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
@@ -34,17 +54,34 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvOption1.setOnClickListener {
-            launchGameFinishedFragment(
-                GameResult(
-                    true,
-                    20,
-                    20,
-                    GameSettings(0, 0, 0, 0)
-                )
-            )
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumber.toString()
+            for (i in 0 until tvOptions.size) {
+                tvOptions[i].text = it.options[i].toString()
+            }
+        }
+        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+        viewModel.enoughCount.observe(viewLifecycleOwner) {
+            val colorResid = if (it) {
+                android.R.color.holo_green_light
+            } else {
+                android.R.color.holo_red_light
+            }
+            val color = ContextCompat.getColor(requireContext(), colorResid)
+            binding.tvAnswersProgress.setTextColor(color)
+        }
+        viewModel.enoughPercent.observe(viewLifecycleOwner) {
+
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
